f80a7c05d20852073eef625828f7e6ad4e1b9289
cozyhorse <mo.apiban@gmail.com>
2024-02-20 11:15:19 +0100

    added icons and image

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,16 +1,19 @@
 import BitcoinLogo from "../../assets/img/Bitcoin-logo.svg";
+import chip from "../../assets/img/chip-transformed.png"
 import ChainLogo from "../../assets/img/chain-logo.svg";
 import Group from "../../assets/img/Group.svg";
 import Subtract from "../../assets/img/Subtract.svg";
 import "./Card.scss"
+import { FaWifi } from "react-icons/fa6";
 
 const Card = ({ id, cardnumber, cardholder, date, vendor, color, backgroundColor }) => {
   return (
     <div className="card-container" key={id} style={{backgroundColor: backgroundColor, color: color}}>
       <div className="card-container__row">
         <div>
-          <img src={BitcoinLogo} alt="" />
-          <div className="chip"><img src={BitcoinLogo} alt="" /></div>
+        <FaWifi className="icon" />
+          <div className="chip"><img src={chip} alt="" /></div>
+          
         </div>
         <div>
         <img src={BitcoinLogo} alt="" />
