eab61fcd9d4b1d5af1b6907047d127d42b8fa64e
pettersson932 <144891931+pettersson932@users.noreply.github.com>
2024-02-20 13:13:52 +0100

    correct icon to cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,26 +1,54 @@
-import BitcoinLogo from "../../assets/img/Bitcoin-logo.svg";
-import chip from "../../assets/img/chip-transformed.png"
-import ChainLogo from "../../assets/img/chain-logo.svg";
+import Bitcoin from "../../assets/img/Bitcoin-logo.svg";
+
+import Blockchain from "../../assets/img/chain-logo.svg";
+
+import Evilcorp from "../../assets/img/Bitcoin-logo.svg";
+import Ninja from "../../assets/img/Bitcoin-logo.svg";
+
+import chip from "../../assets/img/chip-transformed.png";
+
 import Group from "../../assets/img/Group.svg";
 import Subtract from "../../assets/img/Subtract.svg";
-import "./Card.scss"
+import "./Card.scss";
 import { FaWifi } from "react-icons/fa6";
 
-const Card = ({ id, cardnumber, cardholder, date, vendor, color, backgroundColor }) => {
+const Card = ({
+  id,
+  cardnumber,
+  cardholder,
+  date,
+  vendor,
+  color,
+  backgroundColor,
+}) => {
+  let iconSrc = "";
+  if (vendor === "ninja") {
+    iconSrc = Ninja;
+  } else if (vendor === "evilcorp") {
+    iconSrc = Evilcorp;
+  } else if (vendor === "blockchain") {
+    iconSrc = Blockchain;
+  } else {
+    iconSrc = Bitcoin;
+  }
   return (
-    <div className="card-container" key={id} style={{backgroundColor: backgroundColor, color: color}}>
+    <div
+      className="card-container"
+      key={id}
+      style={{ backgroundColor: backgroundColor, color: color }}
+    >
       <div className="card-container__row">
         <div>
-        <FaWifi className="icon" />
-          <div className="chip"><img src={chip} alt="" /></div>
-          
+          <FaWifi className="icon" />
+          <div className="chip">
+            <img src={chip} alt="" />
+          </div>
         </div>
         <div>
-        <img src={BitcoinLogo} alt="" />
+          <img src={iconSrc} alt="" />
         </div>
-        
       </div>
-      
+
       <p className="card-number">{cardnumber}</p>
       <div className="card-container__row">
         <div className="card-container__column">
